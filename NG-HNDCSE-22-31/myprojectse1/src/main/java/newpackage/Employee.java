01091574aa6eb758273d8c0514a512fd72aa18a6
malith perera <udaramalith07@gmail.com>
2020-11-28 11:56:39 +0530

    Netbeans project pull request

---
+++ NG-HNDCSE-22-31/myprojectse1/src/main/java/newpackage/Employee.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author sithumini
+ */
+public class Employee extends Person{
+    private String empNo;
+    
+    public Employee(){}
+
+    public String getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(String empNo) {
+        this.empNo = empNo;
+    }
+    
+    
+}
