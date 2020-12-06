a92af9a36391aa96dcb14c9cbe9ba8fa4a277699
Tharik Kanaka <tharik.kanaka@gmail.com>
2020-12-06 14:18:53 +0530

    Include interface and abstract class work

---
+++ tharik/project1/src/main/java/app/App.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import business.BusinessOperations;
+import db.DBUtil;
+import db.OracleUtil;
+
+/**
+ *
+ * @author tharik
+ */
+public class App {
+    public static void main(String [] args) {
+        
+        DBUtil util = new OracleUtil();
+        
+        BusinessOperations operations = new BusinessOperations();
+        operations.process(util);
+        
+    }
+}
